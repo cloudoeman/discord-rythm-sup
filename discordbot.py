ec0f68e021c425e2157f903499eadec6609071cc
cloudoeman <47080080+cloudoeman@users.noreply.github.com>
2020-07-19 20:01:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,16 +3,12 @@ import os
 import traceback
 import random
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
+    
+@client.event
 async def on_message(ctx):
     if ctx.author.bot:
       return
@@ -20,10 +16,7 @@ async def on_message(ctx):
     if ctx.content == 't':
       await ctx.channel.send('test')
     
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
+
 
     
-bot.run(token)
+client.run(token)
