cad5249b81bce893ccb746a1d5406ffe21bc5af2
cloudoeman <47080080+cloudoeman@users.noreply.github.com>
2020-07-19 22:26:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,5 +26,59 @@ async def on_message(ctx):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+    
+@bot.command()
+async def syamu(ctx):
+    await ctx.send('ウイイイイイイイッッッッス。どうも、シャムでーす。\
+まぁ今日はオフ会、当日ですけども。\
+えーとですね、まぁ集合場所の、えーイオンシネマに行ってきたんですけども、ただいまの時刻は1時を回りました。\
+はい、ちょっと遅れて来たんですけどもね。えー11時ちょっとすぎくらいに、えーイオンシネマに行ったんですけども。\
+ほんでーまぁイオンシネマの全体の動画を撮った後に行ったんですけども。スィー。\
+ほんでーかれこれまぁ2時間くらい、えー待ったんですけども参加者は誰一人来ませんでした。ガチャ。\
+誰一人来ることなかったですぅ。残念ながら。はい。\
+一人くらい来るやろうなーと思ってたんですけども、スゥー、結局2時間くらい待っても誰一人来ませんでしたね、えぇ。\
+でもね、でもオフ会のお知らせの動画にちらほらコメントあったんですけどもね。えー参加者の方の。\
+なんだろう。なんで来なかったんでしょうかねー。\
+まぁーもう一時間くらい待とうと思ったんですけども今日はね、えーまぁみんなとお昼ごはん食べるつもりだったし、\
+あっ夜ご飯もみんなと一緒に食べようかなと思ってたんで今日は朝、パン一枚でございます。\
+今日の朝食はパン一枚！\
+なのでもう1時間待とうと思ったけどさすがにちょっと腹ペコなんで、えー今回のオフ会は残念ながら、こういう悲しい結果で終わりですね。')     
+
+    
+@bot.command()
+async def pas(ctx):
+    await ctx.send('グラップルを使うよ！')
+    
+    
+@bot.command()
+async def rcmm(ctx):
+    await ctx.send('たまたま…たまたま…あの…俺が、おすすめ…俺らがおすすめ…私、我が家…がおすすめしますぅぅう')
+    a = random.randint(0,3)
+    if a == 0:
+      await ctx.send('https://www.youtube.com/watch?v=H6ZlVto_oCs')
+    elif a == 1:
+      await ctx.send('https://www.youtube.com/watch?v=aokzZvBxHIU')
+    elif a == 2:
+      await ctx.send('https://www.youtube.com/watch?v=Qws2n9uBHaE')
+    elif a == 3:
+      await ctx.send('https://www.youtube.com/watch?v=D0GeBHWQtKY')
+    await ctx.send('おもしろいけど、おもしろい')
+    
+
+@bot.command()
+async def dsp(ctx):
+    flag = True
+    tt = 1
+    while flag:
+        async for m in ctx.history(limit=tt):
+            if m.content[0:len("https://www.youtube")] == 'https://www.youtube' and m.author.bot:
+                await m.delete()
+                flag = False
+                await ctx.send('これおすすめなんかな？わからんわ、確信がないわ、紹介するのやめとくわ')
+
+            else:
+                tt += 1    
+    
     
 bot.run(token)
